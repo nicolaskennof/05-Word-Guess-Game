0a07ad8ed80c20b0aa08684517363e0f37e79102
Nicolas <nicolas.kennof@gmail.com>
2018-09-08 14:57:51 -0500

    Pre-release

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Word-Guess-Game
-Simple Hangman Game in order to showcase the Javascript skills
+Simple Hangman Game with pizza ingredients.
+
+Table of contents:
+1. What is the game about?
+2. What are the rules?
+3. How to get started?
+4. Who maintains and contributes?
+
+========== 1. What is the game about? ==========
+The hangman is about finding the hidden word by guessing letters.
+
+========== 2. What are the rules? ==========
+a. Get started by pressing any letter key.
+b. Special characters and non english letters are not applying.
+c. The user 13 guesses in order to discover the whole word.
+d. Once the whole word is discovered, the game starts again: press a new letter to start again.
+e. Once the user do not have any guesses left and the word is not discovered, the game starts again by pressing a new letter again.
+f. If a letter repeats itself in a word, all occurences will be shown.
+
+========== 3. How to get started? ==========
+Please visit https://www.nicolaskennof.github.io/Word-Guess-Game
+
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,136 @@
+var wordlist = [
+        "DOUGH",
+        "CHEESE",
+        "TOMATO",
+        "MOZZARELLA",
+        "FLOUR",
+        "PEPPERONI",
+        "PINEAPPLE",
+        "ARTICHOKE",
+        "YEAST",
+        "JALAPENO",
+        "ONION",
+        "BASIL",
+        "BACON",
+        "SAUSAGE",
+        "HAM",
+        "MUSHROOMS",
+        "CHICKEN",
+        "HABANERO",
+        "CORN",
+        "BEEF",
+        "GARLIC"
+    ];
+
+    var hiddenWord = [];
+    var keyhistory = [];
+    var wins = 0;
+        console.log(userkey);
+        console.log(wins);
+        console.log(remains);
+    var winsCounter = document.getElementById("winsCounter");
+        winsCounter.textContent = wins;
+    var wordoc = document.getElementById("word");
+    var remainsCounter = document.getElementById("remainsCounter");
+    var userkeylist = document.getElementById("userkeylist");
+
+var remains = 13;
+// in order that the computer chooses randomly a string in var ingredients
+var word = wordlist[Math.floor(Math.random() * wordlist.length)];
+console.log(word);
+
+// in order to change every letter of the string by a "_"
+    for ( var i = 0; i < word.length; i++) {
+        hiddenWord[i] = " _ ";
+   };
+    
+console.log(hiddenWord);
+
+// in order to detect the key the user presses
+document.onkeyup = function(event) {
+    var userinput = event.key;
+    var userkey = userinput.toUpperCase();
+    console.log(userkey);
+   
+
+// if the key was already pressed, show alert
+var boolkey = keyhistory.includes(userkey);
+console.log(boolkey);
+
+if ((boolkey) && (keyhistory.length > 0)) {
+
+            alert("You already guessed " + userkey + " earlier!");
+        }
+
+// if not, check if the userkey is part of the word
+    else {
+        keyhistory += userkey + " ";
+        console.log(keyhistory);
+        userkeylist.textContent = keyhistory;
+        var bool = word.includes(userkey);
+        console.log(bool);
+            if (bool === true || bool === false) {
+                remains--;
+                remainsCounter.textContent = remains;
+                wordoc.textContent = hiddenWord;
+                console.log(remains);
+            };
+
+            if (bool) {
+                for (var j = 0; j < word.length; j++) {
+
+// if yes, then replace "_" by userkey
+                    if (word[j] === userkey) {
+                        hiddenWord[j] = " " + userkey + " ";
+                        wordoc.textContent = hiddenWord;
+                        }
+
+
+// if not, do nothing
+                    else {
+                        }
+                    };
+                };
+        }
+
+// if word incomplete AND remaining guesses = 0 then alert GAME OVER AND start new game
+function image () {
+    document.getElementById("img").src = "assets/images/" + word.toLowerCase() + ".jpg";
+}
+
+if ((hiddenWord.includes(" _ ")) && (remains === 0)) {
+    console.log(hiddenWord.includes(" _ "));
+    var audioLoses = new Audio("assets/sounds/lost.mp3");
+    audioLoses.play();
+    image();
+    alert("Game Over!");
+    alert ("Press any key to get started with the Pizza Hangman!")
+    reset();
+}
+
+// if word complete AND remaining guesses > -1 then WINS +1
+    else if ((hiddenWord.includes(" _ ") === false) && (remains > -1)) {
+    console.log(hiddenWord.includes(" _ "));
+    var audioWins = new Audio("assets/sounds/wins.mp3");
+    audioWins.play();
+    image();
+    alert("You win! The word your discovered is " + word);
+    wins++;
+    winsCounter.textContent = wins;
+    alert ("Press any key to get started with the Pizza Hangman!")
+    reset ();
+}
+};
+// Reset
+function reset () {
+    hiddenWord = [];
+    keyhistory = [];
+    remains = 13;
+    word = wordlist[Math.floor(Math.random() * wordlist.length)];
+    console.log(word);
+    for ( var i = 0; i < word.length; i++) {
+        hiddenWord[i] = " _ ";
+   };
+};
+
+    
